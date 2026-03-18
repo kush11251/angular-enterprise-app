8055a3881eda154fb8266b0931cc39133b7f0088
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 02:09:31 +0530

    feat: integrate src/components/ui/buttons/primary-button/primary-button.component.ts

---
+++ src/components/ui/buttons/primary-button/primary-button.component.ts
@@ -0,0 +1,8 @@
+// Primary button component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appPrimaryButton]',
+  template: '<button>Click me!</button>',
+})
+export class PrimaryButtonComponent {}
