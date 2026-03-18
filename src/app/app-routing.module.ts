9d45b488fff9cf2c4dcb0db32bed0e168f143898
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 02:09:26 +0530

    feat: integrate src/app/app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+// App routing
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+
+const routes: Routes = [
+  { path: 'users', component: UserComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
